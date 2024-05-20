f8e81d70fcd5652129d9324203ed746d38456053
Hassan Shams <138686748+danhas11@users.noreply.github.com>
2024-05-20 13:39:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerText = timeString;
+}
+
+// Call updateClock every second
+setInterval(updateClock, 1000);
+
+// Initial call to display the clock immediately
+updateClock();
